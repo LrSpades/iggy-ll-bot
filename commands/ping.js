7b2bd1293b9ef872bc0b5a1e195d6fce06aaafd2
LrSpades <73123077+LrSpades@users.noreply.github.com>
2020-12-03 18:45:44 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,7 @@
 module.exports = {
     name: 'ping',
     description: "this is a ping command!",
-    execute(message, args, client) {
-        const ping = Math.round(client.ws.ping);
-		message.channel.send(`**Pong!**
-Current ping is ${ping}ms`);
+    execute(message,) {
+		message.channel.send(`**Pong!**`);
     }
 }
