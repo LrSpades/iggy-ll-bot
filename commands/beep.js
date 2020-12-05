87ef6a91d38a5cce7c87f15e6c147329d24207eb
LrSpades <73123077+LrSpades@users.noreply.github.com>
2020-12-04 19:34:43 -0600

    Revert "Adding Database"
    This reverts commit 9dddcbce463d3a189d278c18e648760f01b241a3.

--- commands/beep.js
+++ commands/beep.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'beep',
     description: "this is a beep command!",
-    execute(message) {
+    execute(message, args, client) {
         message.channel.send('Boop.');
     }
 }
--- commands/ping.js
+++ commands/ping.js
@@ -2,8 +2,6 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message,) {
-      const ping = Math.round(client.ws.ping);
-      message.channel.send(`**Pong!**
-  Current ping is ${ping}ms`);
+		message.channel.send(`**Pong!**`);
     }
 }
