43402ec9298a37bf24d32bb5322ca3cbefb161c8
LrSpades <73123077+LrSpades@users.noreply.github.com>
2020-12-05 20:36:04 -0600

    1.0.0

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on('guildMemberAdd', member => {
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const list = client.guilds.cache.get("myServerID"); 
+	const list = client.guilds.cache.get("751090237651943556"); 
 	list.members.cache.forEach(member => console.log(member.user.username));
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
