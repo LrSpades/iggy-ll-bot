4c8fd19bfa0a6da80977d417d80cdf66e5458b37
LrSpades <73123077+LrSpades@users.noreply.github.com>
2020-12-07 13:07:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,67 @@ const prefix = '.'
 const fs = require('fs');
 const Data = require('./dbInit.js')
 
+const content = fs.readFileSync('./data/users.txt', {encoding:"utf8"});
+const ids = content.split("\n");
+
+const bals = fs.readFileSync('./data/balance.txt', {encoding:"utf8"})
+const balances = bals.split("\n")
+
+function User (id, balance, position) {
+    this.balance = Number(balance);
+    this.position = position;
+    this.id = id;
+}
+
+let counter = -1;
+const members = ids.map(member => {
+    counter++
+    return member = new User(member, balances[counter], counter)
+})
+
+
+const Data = {
+    Users: {
+             get(id){
+             let user = "nouserfound";
+             members.forEach(player => {
+                    if(player.id != id) return;
+                    user = player;
+                })
+            return user;
+            }
+    }
+}
+
+module.exports = {
+    Users: {
+        get(id){
+            let user = "nouserfound";
+            members.forEach(player => {
+                   if(player.id != id) return;
+                   user = player;
+               })
+            return user;
+            }
+    },
+    System: {
+        Sync() {
+            const syncBalance = [];
+            members.forEach(member => {
+                syncBalance.push(member.balance);
+            })
+            const newBalanceData = syncBalance.join("\n")
+            fs.writeFile('./data/balance.txt', newBalanceData, (err) => {
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log('Succesfully synced database.')
+                }
+            })
+        },
+    }
+}
+
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
