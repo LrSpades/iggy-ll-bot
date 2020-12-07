882f273fae1bbdc41daece3d4549957438bda6b3
LrSpades <73123077+LrSpades@users.noreply.github.com>
2020-12-07 12:28:38 -0600

    1.1.0

--- commands/bal.js
+++ commands/bal.js
@@ -1,9 +1,7 @@
-const Data = require('../dbInit.js')
-
 module.exports = {
     name: 'bal',
     description: "Check your balance!!!",
-    execute(message, client, args, Data) {
+    execute(message, Data) {
         const user = message.member.id;
         console.log(user)
         const balance = Data.Users.get(user).balance;
