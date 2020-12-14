182110b27925145fb8937ae76d451106fedc200b
LrSpades <73123077+LrSpades@users.noreply.github.com>
2020-12-14 11:28:21 -0600

    2.0.0

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const prefix = '.'
 const fs = require('fs');
 const Data = require('./dbInit.js')
+let beanStatus = true;
 
 client.commands = new Discord.Collection();
 
@@ -35,84 +36,118 @@ client.on("guildMemberRemove", (member) => {
 })
 
 client.on('message', message => {
-	if(message.author.bot || message.channel.type === "dm") return;
-	const income = 1
-	Data.Users.get(message.member.id).balance += income;
-
-	if (!message.content.startsWith(prefix)) return;
-
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+	if(message.guild.id === "751090237651943556") {
+		if(message.author.bot || message.channel.type === "dm") return;
+		const income = 1
+		Data.Users.get(message.member.id).balance += income;
+	
+		if (!message.content.startsWith(prefix)) return;
+	
+		
+	
+		if (!cooldowns.has(command.name)) {
+			cooldowns.set(command.name, new Discord.Collection());
+		}
+	
+		const now = Date.now();
+		const timestamps = cooldowns.get(command.name);
+		const cooldownAmount = (command.cooldown || 3) * 1000;
+	
+		if (timestamps.has(message.author.id)) {
+			const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+	
+			if (now < expirationTime) {
+				const timeLeft = (expirationTime - now) / 1000;
+				return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+			}
+		}
+	
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	
+	
+		if (command === 'ping') {
+			client.commands.get('ping').execute(message, client);
+		}
+		else if (command === 'beep') {
+			client.commands.get('beep').execute(message);
+		}
+		else if (command === 'pet') {
+			// Not yet working with data
+		}
+		else if (command === 'random') {
+			client.commands.get('random').execute(message, args);
+		}
+		else if(command === 'avatar') {
+			client.commands.get('avatar').execute(message);
+		}
+		else if (command === 'prune') {
+			client.commands.get('prune').execute(message, args);
+		}
+		else if (command === 'say') {
+			client.commands.get('say').execute(message, args);
+		}
+		else if (command === "ban") {
+			client.commands.get('ban').execute(message);
+		}
+		else if(command === "kick") {
+			client.commmands.get('kick').execute(message);
+		}
+		else if(command === "mute") {
+			client.commands.get('mute').execute(message);
+		}
+		else if(command === 'unmute') {
+			client.commands.get('unmute').execute(message);
+		}
+		else if (command === 'set' && message.author.id === '745478694906101871') {
+			client.commands.get('set').execute(message, args, client);
+		}
+		else if (command === 'bonk') {
+			client.commands.get('bonk').execute(message);
+		}
+		else if (command === "bal" || command === "balance") {
+			client.commands.get('bal').execute(message, Data);
+		}
+		else if (command === "countbal" && message.member.id === "632260979148718084") {
+			client.commands.get('countbal').execute(client, Data)
+		}
+		else if (command === "daily") {
+			client.commands.get('daily').execute(message, Data)
 		}
 	}
+	else if (message.guild.id === "571602097695358986") {
+		if(command === "donate") {
+			const userID = message.author.id
+			const targetID = message.mentions.first().id
+			const transaction = client.users.cache.get('632260979148718084')
 
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-
-	if (command === 'ping') {
-		client.commands.get('ping').execute(message, client);
+			let filter = m => m.author.id === message.author.id
+				transaction.send(`-10 ${userID}
+10 ${targetID}`).then(() => {
+  			    message.channel.awaitMessages(filter, {
+   			       max: 1,
+   			       time: 3000,
+   			       errors: ['time']
+  		      })
+   		     .then(message => {
+     		    message = message.first()
+    		    if (message.content.toUpperCase() == 'SUCCESSFUL') {
+					client.commands.get('donate').execute();
+        		} else if (message.content.toUpperCase() == 'FAILED') {
+					message.channel.send('Failed to execute command, you might not have enough cookies to use this command!')
+				}
+        	})
+        .catch(collected => {
+			beanStatus = false;
+            message.channel.send('Timeout');
+        });
+    })
+		}
 	}
-	else if (command === 'beep') {
-		client.commands.get('beep').execute(message);
-	}
-	else if (command === 'pet') {
-		// Not yet working with data
-	}
-	else if (command === 'random') {
-		client.commands.get('random').execute(message, args);
-	}
-	else if(command === 'avatar') {
-		client.commands.get('avatar').execute(message);
-	}
-	else if (command === 'prune') {
-		client.commands.get('prune').execute(message, args);
-	}
-	else if (command === 'say') {
-		client.commands.get('say').execute(message, args);
-	}
-	else if (command === "ban") {
-		client.commands.get('ban').execute(message);
-	}
-	else if(command === "kick") {
-		client.commmands.get('kick').execute(message);
-	}
-	else if(command === "mute") {
-		client.commands.get('mute').execute(message);
-	}
-	else if(command === 'unmute') {
-		client.commands.get('unmute').execute(message);
-	}
-	else if (command === 'set' && message.author.id === '745478694906101871') {
-		client.commands.get('set').execute(message, args, client);
-	}
-	else if (command === 'bonk') {
-		client.commands.get('bonk').execute(message);
-	}
-	else if (command === "bal" || command === "balance") {
-		client.commands.get('bal').execute(message, Data);
-	}
-	else if (command === "countbal" && message.member.id === "632260979148718084") {
-		client.commands.get('countbal').execute(client, Data)
-	}
-	else if (command === "daily") {
-		client.commands.get('daily').execute(message, Data)
-	}
+	
 });
 
 client.login("Nzg0MTM5OTUwMzE2MDYwNzAy.X8k9PA.FepGfpmZDfM9pJVuVnGzjYRUNmk");
