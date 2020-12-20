fe4d7e6915f7ccb25e47191fdc160afd15510bc9
LrSpades <73123077+LrSpades@users.noreply.github.com>
2020-12-19 19:37:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = 'b.'
 const fs = require('fs');
 const Data = require('./dbInit.js')
 let beanStatus = true;
