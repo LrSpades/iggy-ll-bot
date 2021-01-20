82db49dd50b5994d8060c6aea0bb21134be67c77
LrSpades <73123077+LrSpades@users.noreply.github.com>
2021-01-20 17:45:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -225,8 +225,11 @@ Play a game of Texas Holdem poker!
 		}
 		else if(command === "countdown"){
 			message.channel.send('The countdown will start at 10 and end after 1, when I say GO!')
-			for(i = 10; i > 0; i--){
+			for(let i = 10; i > 0; i--){
 				setTimeout(() => {
+					if(i === 0){
+						message.channel.send('GO!!')
+					}
 					message.channel.send(i);
 				}, 1000)
 			}
