0d4a4c0586d1f2f58c3d1a812e2006d1f4370e6a
LrSpades <73123077+LrSpades@users.noreply.github.com>
2021-02-04 15:09:06 -0600

    3.1.1

--- commands/Misc/say.js
+++ commands/Misc/say.js
@@ -3,7 +3,7 @@ module.exports = {
     expectedArgs: '<text>',
     callback: (client, message, args, text, DataScrapper) => {
         message.channel.send(text);
-        message.delete(0);
+        message.delete({ timeout: 0 });
     },
     permissions: ['BAN_MEMBERS'],
 };
