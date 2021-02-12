643387907a06dbe4061cadbf3f4ac1f69149421e
LrSpades <73123077+LrSpades@users.noreply.github.com>
2021-02-12 14:29:44 -0600

    Update command-base.js

--- commands/command-base.js
+++ commands/command-base.js
@@ -100,8 +100,9 @@ module.exports = (client, commandOptions) => {
                     }
                 }
 
-                if(message.author.id !== Settings.owner.user.id && testing) {
-                    console.log(message.author + Settings.owner.user);
+                if(message.author !== Settings.owner.user.id && testing) {
+                    console.log(message.author);
+                    console.log(Settings.owner.user.id);
                     return message.reply('Listen here you piece of \\:poop: i bet its you pixel. I made a testing feature so ppl like you dont fing use my commands WHEN IM TESTING THEMMMMM');
                 }
 
