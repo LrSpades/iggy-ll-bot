b4f79ed611e9f9429a45d4bd783af0c4ff4b1b8e
LrSpades :D <73123077+LrSpades@users.noreply.github.com>
2021-02-23 09:33:41 -0600

    Update boof.js

--- commands/Troll/boof.js
+++ commands/Troll/boof.js
@@ -3,7 +3,7 @@ module.exports = {
     expectedArgs: '<member>',
     minArgs: 1,
     maxArgs: 1,
-    callback: (Settings, message, args, text) => {
+    callback: async (Settings, message, args, text) => {
         const member = message.member;
         const target = message.mentions.members.first();
         const Bean = Settings.client.channels.cache.get('790023648064700436');
@@ -15,20 +15,15 @@ module.exports = {
 
         message.delete();
 
-        message.channel.send(`You monster... BOOFED ${target.username} -${cost}:cookie:`);
+        message.channel.send(`You monster... BOOFED ${target} -${cost}:cookie:`);
 
-        const checkForTyping = setInterval(() => {
-            Settings.client.on('typingStart', (channel, user) => {
-                setTimeout(() => {
-                    member.roles.add(mutedRole).catch(err => {console.error(err)});
-                    setTimeout(() => {
-                        member.roles.remove(mutedRole).catch(err => {console.error(err)});
-                    }, 1000)
-                }, 1000);
-                
-                return
-            });
-        }, 1000 * 5);
+        Settings.client.on('startTyping', (channel, user) => {
+            if(target.id !== user.id) return
+            
+            target.roles.add(mutedRole).catch(err => conosle.error(err));
+            await setTimeout(() => {target.roles.add(mutedRole).catch(err => conosle.error(err))}, 1000);
+            return
+        });
     },
     servers:['571602097695358986'],
     testing: true,
