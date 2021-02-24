4308f30de16722581c19d7658bc9eeecef7311bc
LrSpades :D <73123077+LrSpades@users.noreply.github.com>
2021-02-24 14:06:50 -0600

    3.4.2
    Added cost to cd because of spammers. At least this way I will become rich.

--- commands/Utilities/countdown.js
+++ commands/Utilities/countdown.js
@@ -4,26 +4,30 @@ module.exports = {
     minArgs: 1,
     maxArgs: 1,
     callback: (Settings, message, args, text) => {
+        const user = message.author;
+        const cost = 4;
+        originChannel = message.channel;
         let count = args.shift();
-
         const maxAmount = 20;
         const minAmount = 5;
 
         if(count > maxAmount || count <= minAmount) return message.reply(`Please choose a number between ${maxAmount} and ${minAmount}...`);
 
-        const counter = setInterval(() => {
-            if (count > 0) {
-                message.reply(count);
-                count--;
-            }
-            else if (count === 0) {
-                message.reply('GOO!!!');
-                count--;
-            }
-            else {
-                clearInterval(counter);
-                return;
-            }
-        }, 1000);
+        Settings.Utils.BeanTXN({ cost: cost, origin: originChannel, userId: user.id }).then(result => {
+            const counter = setInterval(() => {
+                if (count > 0) {
+                    message.reply(count);
+                    count--;
+                }
+                else if (count === 0) {
+                    message.reply('GOO!!!');
+                    count--;
+                }
+                else {
+                    clearInterval(counter);
+                    return;
+                }
+            }, 1000);
+        });
     },
 };
