904df08fa04572d50dba86b8cc1f0138a0ec15d0
LrSpades :D <73123077+LrSpades@users.noreply.github.com>
2021-03-12 14:20:48 -0600

    Update getKanji.js

--- getKanji.js
+++ getKanji.js
@@ -7,28 +7,28 @@ async function getKanji() {
   difficulties.forEach(async (df) => {
     const uri = `https://www.wanikani.com/kanji?difficulty=${df}`;
     const response = await got(uri);
-    const $ = cheerio.load(response.body);
+    let $ = cheerio.load(response.body);
     
     const $kanjiCollection = $('.character');
     
     const values = $kanjiCollection.toArray().map(async (kanji) => {
       const kanjiUri = `https://www.wanikani.com/kanji/${kanji.text()}`;
       const kanjiPage = await got(kanjiUri);
-      const T = cheerio.load(response.body);
+      $ = cheerio.load(response.body);
       
       let kanjiObject = {
       };
       
       const $kanjiInfo = T('h1:not(a > h1), .alt-character-list, .alternative-meaning:not(.user-synonyms), .mnemonic-content, .span4').toArray();
       
-      kanjiObject.kanji = T($kanjiInfo.shift()).text();
-      kanjiObject.radicalCombo = T($kanjiInfo.shift()).text();
-      kanjiObject.meanings = T($kanjiInfo.shift()).text();
-      kanjiObject.mnemonic = T($kanjiInfo.shift()).text();
-      kanjiObject.onyomi = T($kanjiInfo.shift()).text();
-      kanjiObject.kunyomi = T($kanjiInfo.shift()).text();
-      kanjiObject.nanori = T($kanjiInfo.shift()).text();
-      kanjiObject.readingsMnemonic = T($kanjiInfo.shift()).text();
+      kanjiObject.kanji = $($kanjiInfo.shift()).text();
+      kanjiObject.radicalCombo = $($kanjiInfo.shift()).text();
+      kanjiObject.meanings = $($kanjiInfo.shift()).text();
+      kanjiObject.mnemonic = $($kanjiInfo.shift()).text();
+      kanjiObject.onyomi = $($kanjiInfo.shift()).text();
+      kanjiObject.kunyomi = $($kanjiInfo.shift()).text();
+      kanjiObject.nanori = $($kanjiInfo.shift()).text();
+      kanjiObject.readingsMnemonic = $($kanjiInfo.shift()).text();
       
       return kanjiObject;
     });
